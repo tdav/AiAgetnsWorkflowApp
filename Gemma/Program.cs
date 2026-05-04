aa514671db86aefff8933814378a5b3e359a62a9
admin <tdav@mail.ru>
2026-05-04 13:38:45 +0500

    1

---
+++ Gemma/Program.cs
@@ -0,0 +1,18 @@
+﻿using LMKit.Model;
+using LMKit.Integrations.ExtensionsAI.ChatClient;
+using Microsoft.Extensions.AI;
+
+// Load a model
+using var model = LM.LoadFromModelID("gemma4:e2b");
+
+// Create an IChatClient
+IChatClient client = new LMKitChatClient(model);
+
+// Use it
+await foreach (var update in client.GetStreamingResponseAsync("создать c# код которвый аудио переобразование фуре"))
+{
+    Console.Write(update.Text);
+}
+
+Console.WriteLine();
+Console.ReadKey();
