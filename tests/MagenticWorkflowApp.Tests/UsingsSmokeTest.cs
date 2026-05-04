8ea5a0af90f26f3faf3f8240c3a5c48c8590e60e
admin <tdav@mail.ru>
2026-05-04 18:10:09 +0500

    test: scaffold MagenticWorkflowApp.Tests xUnit project

---
+++ tests/MagenticWorkflowApp.Tests/UsingsSmokeTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace MagenticWorkflowApp.Tests;
+
+public class UsingsSmokeTest
+{
+    [Fact]
+    public void TestProjectCompilesAndDiscoversTests()
+    {
+        Assert.True(true);
+    }
+}
